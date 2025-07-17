67d8c85d301bcfd941dbaa55026b14a8587272be
Rishabh Kumar Chauhan <111337018+Rishabh2482@users.noreply.github.com>
2025-07-18 02:11:51 +0530

    Added  AddUser functionality

--- Frontend/src/Components/AddUser.jsx
+++ Frontend/src/Components/AddUser.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { useState} from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function AddUser() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const navigate = useNavigate();
+
+  const addUser = async(e)  => {
+    e.preventDefault();
+
+    try{
+          const response =await axios.post('http://localhost:5000/users',{
+              name,
+              email,
+              city
+            })
+            console.log(response);
+          navigate('/');
+    }catch(error){
+      console.log("error occured whild adding data", error)
+    }
+  }
+
   return (
-    <div>
-      
+    <div className="columns mt-5">
+        <div className="column is-half">
+            <form onSubmit={(e)=>addUser(e)}>
+              <div className="field">
+                <label htmlFor="name" className="label"> Name</label>
+                <input type="text" value={name} 
+                        className="input"
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter name"
+                />
+              </div>
+
+              <div className="field">
+                <label htmlFor="email" className="label"> Email</label>
+                <input type="email" value={email} 
+                        className="input"
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter email"
+                />
+              </div>
+
+              <div className="field">
+                <label htmlFor="city" className="label"> City</label>
+                <input type="city" value={city} 
+                        className="input"
+                        onChange={(e) => setCity(e.target.value)}
+                        placeholder="Enter city"
+                />
+              </div>
+
+              <div className="field">
+                  <div className="control">
+                    <button type="submit" className="button is-success">
+                        Add user
+                    </button>
+                  </div>
+              </div>
+            </form>
+        </div>
     </div>
   )
 }
